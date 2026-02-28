1a0145a1f5a4109ec899e911d0ff25556f477190
XuaNguyen <xuan.0211@gmail.com>
2026-03-01 00:52:08 +0700

    refactor: update LRUCache import from default to named.

--- src/shared/jwt/cache.js
+++ src/shared/jwt/cache.js
@@ -5,7 +5,7 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import LRUCache from 'lru-cache';
+import { LRUCache } from 'lru-cache';
 
 /**
  * In-memory LRU cache for verified JWTs.
